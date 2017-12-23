bed12a3c91f3b1a7c9ffa017e6ac3c18782b62fa
Athar Khan <md.athar.khan96@gmail.com>
2017-12-23 23:58:29 +0530

    Delete urls.py

--- fb_chatbot/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns,url
-from fb_chatbot import views
-from .views import MyQuoteBotView
-
-
-urlpatterns = patterns('',
-	url(r'^$', views.index,name = 'index'),
-	url(r'^facebook_auth/?$', MyQuoteBotView.as_view()))
